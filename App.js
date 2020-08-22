4726b0d2c521bd96e7e3a9a90b55da9cd6ad352d
Sean Sutton <sutton_mjbc@icloud.com>
2020-08-22 19:06:09 -0400

    background color state using hooks

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import RoundedButton from './components/RoundedButton'
 
 export default function App () {
+  const [color, setColor] = useState('black')
+
+  const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    return `rgb(${red}, ${green}, ${blue})`
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: color }]}>
       <Text>Open up App.js to start working on your app!</Text>
       <RoundedButton
         backgroundColor='teal'
         text='Next'
         textColor='white'
-        onPress={() => alert('Pressed!')}
+        onPress={() => setColor(randomRgb())}
       />
       <StatusBar style='auto' />
     </View>
