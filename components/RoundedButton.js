6a9dd493b0e24fabee5e4cef18968a9604fd6f8e
Sean Sutton <sutton_mjbc@icloud.com>
2020-08-22 18:54:48 -0400

    implementing roundedButton

---
+++ components/RoundedButton.js
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+
+export default function RoundedButton (props) {
+  const { text, icon, textColor, backgroundColor, onPress } = props
+  const color = textColor || 'white'
+
+  return (
+    <TouchableOpacity
+      onPress={() => onPress && onPress()}
+      style={[styles.wrapper, { backgroundColor: backgroundColor || 'transparent' }]}
+    >
+      <View style={styles.buttonTextWrapper}>
+        {icon}
+        <Text style={[{ color }, styles.buttonText]}>{text}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 15,
+    display: 'flex',
+    borderRadius: 40,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'white',
+    marginBottom: 15,
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 16,
+    width: '100%',
+    textAlign: 'center'
+  },
+  buttonTextWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  }
+})
